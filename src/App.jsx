e81fa2e009ab39bf7c0224cd8b8b2df910dd8f0a
trantuvan <trantuvan.kan@gmail.com>
2023-01-16 23:04:42 +0700

    react life cycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: true,
+    };
+    this.mountCounter = this.mountCounter.bind(this);
+    this.unmountCounter = this.unmountCounter.bind(this);
+  }
+
+  mountCounter() {
+    this.setState({ mount: true });
+  }
+
+  unmountCounter() {
+    this.setState({ mount: false });
+  }
+
   render() {
+    const { mount } = this.state;
     return (
-      <div className="h-screen flex items-center justify-center">
-        <Counter />
+      <div className="h-screen flex flex-col gap-3 items-center justify-center">
+        <div className="space-x-2">
+          <button
+            type="button"
+            className="text-lg px-2 py-1 bg-indigo-500 rounded hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+            onClick={this.mountCounter}
+            disabled={this.state.mount}
+          >
+            MountCounter
+          </button>
+          <button
+            type="button"
+            className="text-lg px-2 py-1 bg-emerald-500 rounded hover:bg-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+            onClick={this.unmountCounter}
+            disabled={!this.state.mount}
+          >
+            UnmountCounter
+          </button>
+        </div>
+        {mount === true ? <Counter /> : null}
+        {/* <Counter mount={mount} /> */}
       </div>
     );
   }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,14 +2,99 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
   constructor(props) {
+    console.log('Constructor');
     super(props);
     this.state = {
       counter: 0,
     };
+    this.incrementCounter = this.incrementCounter.bind(this);
+    this.decrementCounter = this.decrementCounter.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+    console.log('-----------------');
+  }
+
+  componentDidUpdate(prevProps, prevState, snapShot) {
+    console.log('Component Did Update');
+    console.log('-----------------');
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount');
+    console.log('----------------------');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('Component Did Catch');
+    this.setState({ error, info });
+  }
+
+  incrementCounter() {
+    this.setState(({ counter }) => ({
+      counter: counter + 1,
+    }));
+  }
+
+  decrementCounter() {
+    this.setState(({ counter }) => ({
+      counter: counter - 1,
+    }));
   }
 
   render() {
+    console.log('Render');
     const { counter } = this.state;
-    return <div className="text-3xl">Counter: {counter}</div>;
+    // const { mount } = this.props;
+
+    if (this.state.error) {
+      return <div>We have encounter an error</div>;
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="space-x-2">
+          <button
+            type="button"
+            className="text-lg px-2 py-1 bg-indigo-500 rounded hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+            onClick={this.incrementCounter}
+          >
+            Increment
+          </button>
+          <button
+            type="button"
+            className="text-lg px-2 py-1 bg-emerald-500 rounded hover:bg-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+            onClick={this.decrementCounter}
+          >
+            Decrement
+          </button>
+        </div>
+        <div className="text-3xl">Counter: {counter}</div>
+      </div>
+      //   <>
+      //     {mount === false ? null : (
+      //       <div className="flex flex-col gap-2">
+      //         <div className="space-x-2">
+      //           <button
+      //             type="button"
+      //             className="text-lg px-2 py-1 bg-indigo-500 rounded hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+      //             onClick={this.incrementCounter}
+      //           >
+      //             Increment
+      //           </button>
+      //           <button
+      //             type="button"
+      //             className="text-lg px-2 py-1 bg-emerald-500 rounded hover:bg-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-400"
+      //             onClick={this.decrementCounter}
+      //           >
+      //             Decrement
+      //           </button>
+      //         </div>
+      //         <div className="text-3xl">Counter: {counter}</div>
+      //       </div>
+      //     )}
+      //   </>
+    );
   }
 }
